43d90243cf9eb1dadb49c28b12b6b78a3471294b
Rajendra1238 <152177335+Rajendra1238@users.noreply.github.com>
2026-04-04 09:28:30 +0530

    Initial commit

--- backend/obj/Debug/net8.0/GymBackend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/GymBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+970e4e0142f586d168f1df344e865e2412aca889")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
